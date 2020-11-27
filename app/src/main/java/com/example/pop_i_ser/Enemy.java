b34f69934c70c0f0d654d9bcde0d408678be3d6f
YHNRH <kdog2105@mail.ru>
2020-11-27 14:08:53 +0300

    Health bar added

--- app/src/main/java/com/example/pop_i_ser/Enemy.java
+++ app/src/main/java/com/example/pop_i_ser/Enemy.java
@@ -2,12 +2,14 @@ package com.example.pop_i_ser;
 
 public class Enemy {
     public int health;
+    int max_health;
     public int gold;
     public int stage;
     public Enemy(int stage)
     {
         this.stage = stage;
-        health = stage*10 ;
+        max_health = stage*10 ;
+        health=max_health;
         gold   = stage*5  ;
     }
 
@@ -18,6 +20,11 @@ public class Enemy {
     public int getHealth()
     {
         return health;
+    }
+
+    public int getMaxHealth()
+    {
+        return max_health;
     }
 
 
--- app/src/main/java/com/example/pop_i_ser/MainActivity.java
+++ app/src/main/java/com/example/pop_i_ser/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,14 +16,19 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
     ImageView enemy;
     TextView hp;
+    TextView hb;
     Enemy enemy_o;
     Hero hero = new Hero();
+    float pixels;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
         enemy = findViewById(R.id.enemy);
         hp = findViewById(R.id.hp);
+        hb = findViewById(R.id.hb);
         enemy_o = Location.getEnemy();
         hp.setText(String.valueOf(enemy_o.getHealth()));
         int img = getResources().getIdentifier("drawable/e"+enemy_o.getImage(), null, getPackageName());
@@ -34,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: // нажатие
-                enemy.setLayoutParams(new LinearLayout.LayoutParams(300,300));
+                enemy.setLayoutParams(new LinearLayout.LayoutParams((int) (20*pixels),(int)(20*pixels)));
                 enemy.setColorFilter(ContextCompat.getColor(this,R.color.no_red));
                 break;
             case MotionEvent.ACTION_MOVE: // движение
@@ -45,14 +52,20 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                     int img = getResources().getIdentifier("drawable/e"+enemy_o.getImage(), null, getPackageName());
                     enemy.setImageResource(img);
                     hp.setText(String.valueOf(enemy_o.getHealth()));
-                    enemy.setLayoutParams(new LinearLayout.LayoutParams(350,350));
+                    enemy.setLayoutParams(new LinearLayout.LayoutParams((int) (34*pixels),(int)(34*pixels)));
+                    LinearLayout.LayoutParams param = new LinearLayout.LayoutParams((int) (enemy_o.getHealth()/enemy_o.getMaxHealth()*34*pixels),(int)(6*pixels));
+                    param.gravity = Gravity.CENTER;
+                   hb.setLayoutParams(param);
                     enemy.setColorFilter(ContextCompat.getColor(this,R.color.no_color));
                 }
                 else {
                     enemy_o.isAttacked(hero.getAttack());
                     hp.setText(String.valueOf(enemy_o.getHealth()));
+                    LinearLayout.LayoutParams param = new LinearLayout.LayoutParams((int) ((float)enemy_o.getHealth()/ (float)enemy_o.getMaxHealth()*34*pixels),(int)(6*pixels));
+                    param.gravity = Gravity.CENTER;
                     enemy.setColorFilter(ContextCompat.getColor(this, R.color.no_color));
-                    enemy.setLayoutParams(new LinearLayout.LayoutParams(350, 350));
+                    hb.setLayoutParams(param);
+                    enemy.setLayoutParams(new LinearLayout.LayoutParams((int) (34*pixels),(int)(34*pixels)));
                 }
                 break;
                     case MotionEvent.ACTION_CANCEL:
